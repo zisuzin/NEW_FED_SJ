cb782d3ca8577d22445a079b32f15e56476be57d
zisuzin <zi_soo_zin@naver.com>
2023-03-15 17:11:45 +0900

    중간푸시

--- 001.HTML학습/09.ShoppingMall_PJ/js/main.js
+++ 001.HTML학습/09.ShoppingMall_PJ/js/main.js
@@ -1,57 +1,63 @@
-// 쇼핑몰 배너 JS - 01.가로방향 배너 슬라이드 //
+// 쇼핑몰 배너 JS - 03.페이드효과 //
 
-// HTML태그 로딩후 loadFn함수 호출! ///
+// 로딩후 loadFn함수 호출 //////////
 window.addEventListener("DOMContentLoaded", loadFn);
 
-// /***************************************************** 
-//     [ 슬라이드 이동 기능정의 ]
-//     1. 이벤트 종류: click
-//     2. 이벤트 대상: 이동버튼(.abtn)
-//     3. 변경 대상: 슬라이드 박스(#slide)
-//     4. 기능 설계:
+/******************************************** 
+ * 
+    [ 페이드 효과 슬라이드 기능정의 ]
+    -> 슬라이드 순번에 대한 전역변수를 사용한다!
 
-//         (1) 오른쪽 버튼 클릭시 다음 슬라이드가
-//             나타나도록 슬라이드 박스의 left값을
-//             -100%로 변경시킨다.
-            slide.style.left = "-100%";
+    1. 오른쪽 버튼클릭시 다음 순번슬라이드에
+    클래스 "on"을 줘서 opacity:1, z-index:1
+    로 보이며 맨위로 설정해준다!(트랜지션적용)
+    ->나머지 li는 모두 "on"을 지워서 초기화!
 
-//         (2) 슬라이드 이동후!! (0.4초후)
-            setTimeout(()=>{
-                slide.appendChild(
-                    slide.querySelectorAllor("li")[0]
-                );
-                // 동시에 left값을 0으로 변경한다.
-            },400);
+    2. 왼쪽버튼은 이전순번이 나오며 위와 동일
 
-//         (3) 공통기능: 슬라이드 위치표시 블릿
-//             - 블릿 대상: .indic li
-//             - 변경 내용: 슬라이드 순번과 같은 순번의
-//             li에 클래스 "on"주기(나머진 빼기->초기화!)
+    3. 끝번호에 가서는 처음은 마지막으로 
+    마지막은 처음으로 슬라이드가 다시 반복된다.
+
+    4. 블릿은 현재 슬라이드와 일치된 순번표시
+
+********************************************/
 
 /****************************************** 
     함수명: loadFn
-    기능: 로딩 후 버튼 이벤트 및 기능구현
+    기능: 로딩후 이벤트설정 및 슬라이드 기능
 ******************************************/
 function loadFn() {
     console.log("로딩완료!");
 
     // 슬라이드번호 변수
     let snum = 0;
-    // 슬라이드 개수
-    let scnt = document.querySelectorAll("#slide>li").length;
-    console.log("슬개수:",scnt);
 
     // 1. 대상선정
     // 1-1. 이벤트 대상: .abtn
     const abtn = document.querySelectorAll(".abtn");
     // console.log(abtn);
 
-    // 1-2. 변경 대상: #slide
-    const slide = document.querySelector("#slide");
+    // 1-2. 변경 대상: #slide > li
+    const slide = document.querySelectorAll("#slide>li");
+    // 슬라이드 개수
+    let scnt = slide.length;
+    console.log("슬개수:",scnt);
+    
+    // 1-3. 블릿 대상: .indic li
+    const indic = document.querySelectorAll(".indic li");
 
     // 2. 슬라이드 변경함수 만들기
     const goSlide = (seq) => {
         console.log("슬고우!",seq);
+
+        // 광클금지변수 : 0 - 허용, 1 - 불허용
+        let prot = 0;
+        // 광클금지 설정하기 //////
+            if (prot) return;
+            prot = 1; // 잠금!
+            setTimeout(() => {
+                prot = 0; // 해제!
+            }, 400); /// 0.4초후 해제! ///
 
         // 1. 방향에 따른 분기
         // 1-1. 오른쪽버튼 클릭시: seq===1일때
@@ -74,11 +80,20 @@ function loadFn() {
         //끝다음->처음
         else if(snum === scnt) snum = 0;
 
-        // 3. 이동하기
-        // 이동대상: slide변수
-        slide.style.left = (snum*-100)+"%";
-        // slide.style.transition = "left .4s ease-in-out";
+        // 3. 이동하기 : 해당순번 슬라이드 li에 클래스"on"넣기
+        // 변경대상: slide변수(#slide li)
+        // 전체초기화!
+        slide.forEach(ele=>ele.classList.remove("on"));
+        // 해당순번li에 클래스넣기
+        slide[snum].classList.add("on");
     }; ////////// goSlide함수 ///////////
+
+    // 4. 블릿변경 : 해당순번 블릿 li에 클래스 "on"넣기
+    // 변경대상: slide변수(.indic li)
+        // 전체초기화!
+        indic.forEach(ele=>ele.classList.remove("on"));
+        // 해당순번li에 클래스넣기
+        indic[snum].classList.add("on");
 
     // 3. 대상에 이벤트 설정하기
     abtn.forEach((ele, idx) => {
@@ -86,5 +101,41 @@ function loadFn() {
             goSlide(idx);
         }; ///// click함수 //////
     }); /////// forEach //////////
-} //////////////// loadFn 함수 ///////////////
-/////////////////////////////////////////////
+
+
+    /******************************** 
+        블릿클릭시 슬라이드 이동하기 
+    ********************************/
+    // 이벤트 대상 : .indic li -> indic변수
+    // 이벤트 종류 : click 
+    indic.forEach((ele,idx)=>{
+        ele.onclick =() =>{
+        // 전역변수 snum 업데이트하기 
+        // 왜? 블릿순번===슬라이드순번 이므로!
+        snum = idx;
+
+        // 전체블릿초기화
+        indic.forEach(ele=>ele.classList.remove("on"));
+
+        // 해당 순번에 클래스 "on"넣기
+        indic[snum].classList.add("on");
+
+        // 5. 자동넘김멈춤 함수호출!
+        clearAuto();
+
+        }; // click 
+    }); // forEach
+
+
+    /************************************ 
+        슬라이드 / 블릿 변경기능 공통함수
+        함수명 : chgSlide
+    ************************************/
+   function chgSlide(obj){ // obj - 변경대상 노드리스트 
+    console.log("공통함수!");
+    obj.forEach((ele)=> ele.classList.remove("on"));
+    // 해당순번li에 클래스넣기
+    obj[snum].classList.add("on");
+   }
+}
+ /////////////// loadFn 함수 //////////////
