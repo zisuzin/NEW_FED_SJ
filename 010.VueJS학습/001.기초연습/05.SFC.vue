3ce5e35ad12b7f0c0bd56c8a76ebb8a61be1d04f
zisuzin <zi_soo_zin@naver.com>
2023-05-01 15:49:56 +0900

    s

---
+++ 010.VueJS학습/001.기초연습/05.SFC.vue
@@ -0,0 +1,24 @@
+<!-- html 템플릿코드 -->
+<template>
+    <div class="mytemp">
+        <span class="tit">{{  }}</span>
+    </div>
+</template>
+
+<!-- 자바스크립트 코드 -->
+<script>
+export default(await import("vue").undefineComponent({
+    name: "myTemp",
+    data(){
+        return {
+            txt: "연습이예요!"
+        }
+    }
+}))
+</script>
+
+<!-- 스타일시트 코드 -->
+<!-- 이 파일에서만 적용되는 요소임을 명시하기 위해 scoped 키워드 필수 입력! -->
+<style scoped>
+    span.tit {color:red};
+</style>
