696c5a79159498e6d29abb3a4727f5e2ecc1e92e
zisuzin <zi_soo_zin@naver.com>
2023-05-01 12:55:01 +0900

    s

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/mainjs/tempData/data-common.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/mainjs/tempData/data-common.js
@@ -1,35 +1,8 @@
-// 파일럿 PJ 메인페이지 JS - main.js
+// 공통 data 객체 셋팅 JS - data-common.js
 
-/************************************************* 
-    [ 메인 페이지 주요 기능 ]
-
-    1. 자동스크롤 기능 구현 (OK)
-    + 페이지 도착후 등장액션 구현
-
-    2. 햄버거버튼 전체메뉴 보이기/숨기기(OK)
-    + 전체메뉴 배경동영상 보일때만 재생(숨길때 멈춤)
-
-    3. 배너 터치기능 넘기기 (제이쿼리 UI이용)
-    + 배너별 타이틀 등장하기
-    + 양쪽 이동버튼 플러그인 적용하기
-
-*************************************************/
-
-// 자동스크롤 기능 함수 가져오기
-import autoScroll from "./jquery-autoScroll.js";
-// 메뉴기능함수 가져오기
-import menuFn from "./mainjs/menu.js";
-// 배너기능함수 가져오기
-import banFn from "./mainjs/ban.js";
-// 공통 데이터 가져오기
-import comData from "./mainjs/tempData/data-common.js";
-
-
-//###### 상단영역 메뉴 뷰 템플릿 셋팅하기 #######
-// Vue.component(내가지은요소명,{옵션})
-Vue.component("top-comp",{
-    template:`
-                    
+const comData = {
+    // 상단영역 html data
+    tarea: `
     <header class="top ibx">
     <!-- 1-1.로고 -->
     <h1 id="logo">
@@ -94,40 +67,26 @@ Vue.component("top-comp",{
         </nav>
     </div>
 </header>
-
     `,
-}); ////////////// 상단영역 Vue component /////////////
+    // 하단영역 html 데이터
+    barea: `
+    <address>
+        서울특별시 강남구 언주로 541 에프앤에프빌딩<br />
+        사업자등록번호 : 214-86-09977 / 통신판매업 신고번호 : 제2011-서울강남-00086호 (주)
+        에프앤에프 / 대표 : 김창수<br />
+        교환·반품 반송처 : (17384) 경기도 이천시 마장면 프리미엄아울렛로 113-37 F&amp;F
+        물류센터 디스커버리 물류부<br /><br />
 
-//###### 상단영역 뷰 인스턴스 생성하기 ##########
-// new Vue({옵션})
-new Vue({
-    el:"#top",
-    data:{},
-    // mounted 실행구역: DOM연결후
-    mounted:function(){
-        // 제이쿼리코드함수 호출!
-        console.log("mounted구역");
-        
-        // 자동스크롤 호출
-        autoScroll();
-        // 메뉴기능 호출
-        menuFn();
-        // 배너기능 호출
-        banFn();
-        
-    },    
-    // created 실행구역 : DOM연결전
-    created:function(){
-        // DOM연결전 데이터 가공작업
-        console.log("created구역");
-    },    
-})    
+        고객센터　<em>080 - 820 - 8802</em> 　　 평일 오전 9시 ~ 오후 6시 (토·일·공휴일
+        휴무) <br />
+        A/S : 02 - 3474 - 8914　 E - MAIL :
+        <a href="mailto:discovery@fnf.co.kr" title="메일 보내기">discovery@fnf.co.kr</a> 　
+        FAX : 02 - 520 - 0991 <br /><br />
 
-// 하단영역 뷰 인스턴스 생성
-new Vue({
-    el:"#info",
-})
+        COPYRIGHT F&amp;F CO.,LTD.ALL RIGHTS RESERVED
+    </address>
+    `,
+}; // comData //
 
-
-
-
+// 내보내기
+export default comData;
---
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/manu.js
@@ -0,0 +1,8 @@
+// 메뉴관련 기능 JS - menu.js
+
+function menuFn(){
+
+} // menuFn 함수 // 
+
+// 함수 내보내기
+export default menuFn;
