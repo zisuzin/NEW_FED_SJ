65ee1723d6f041d1f72eb180658125c4e54377f6
zisuzin <zi_soo_zin@naver.com>
2023-05-02 21:57:26 +0900

    s

--- 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/gallery.js
+++ 700.쉐도잉프로젝트/02.VOGUE_PJ/002.site/js/gallery.js
@@ -1,15 +1,13 @@
-// 보그PJ : 갤러리 페이지 JS - gallery.js
+// 보그 PJ : 갤러리 페이지 JS - gallery.js
 
-$(()=>{ // jQB // 
+$(() => {
+    ///////////// jQB //////////////
 
     let swiper = new Swiper(".mySwiper", {
         slidesPerView: 3, // 화면화면당 슬라이드 개수
-        spaceBetween: 30, // 슬라이드 사이간격(px)
-        pagination: {
-          el: ".swiper-pagination",
-          clickable: true,
-        },
+        spaceBetween: 20, // 슬라이드 사이간격(px)
         slidesPerGroup: 1, // 슬라이드 그룹(개수단위로 넘어감!)
+
         loop: true, // 무한루프(기본값:false)
         loopFillGroupWithBlank: true,
         // 한 화면 단위지정시 단위보다 그룹이 작을 경우 빈칸을 채움
@@ -34,25 +32,26 @@ $(()=>{ // jQB //
         breakpoints: {
             // when window width is >= 200px
             200: {
-            slidesPerView: 1,
-            spaceBetween: 0
+                slidesPerView: 1,
+                spaceBetween: 0,
             },
             // when window width is >= 700px
             700: {
-            slidesPerView: 2,
-            spaceBetween: 30
+                slidesPerView: 2,
+                spaceBetween: 10,
             },
-            // when window width is >= 640px
+            // when window width is >= 1000px
             1000: {
-            slidesPerView: 3,
-            spaceBetween: 40
-            }
-        }
-      });
+                slidesPerView: 3,
+                spaceBetween: 20,
+            },
+        },
+    });
 
-        // 테스트 : 스와이퍼의 인스턴스를 생성후
-        // 특정기능의 속성과 메서드를 사용할 수 있다!
-        // 예) Gallery 라는 제목을 클릭하면 다음 슬라이드 보이기 기능 구현하기
-        $(".stit").click(()=>swiper.slideNext());  
-        
-}); // jQB // 
+
+    // 테스트 : 스와이퍼의 인스턴스를 생성후
+    // 특정기능의 속성과 메서드를 사용할 수 있다!
+    // 예) Gallery라는 제목을 클릭하면 다음 슬라이드 보이기 기능 구현하기
+    $(".stit").click(()=>swiper.slideNext());
+
+}); ////////// jQB ///////////////////
--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/sub.js
@@ -1,26 +1,7 @@
-// 파일럿 PJ 메인페이지 JS - main.js
+// 카테고리 서브페이지 JS - sub.js
 
-/************************************************* 
-    [ 메인 페이지 주요 기능 ]
-
-    1. 자동스크롤 기능 구현 (OK)
-    + 페이지 도착후 등장액션 구현
-
-    2. 햄버거버튼 전체메뉴 보이기/숨기기(OK)
-    + 전체메뉴 배경동영상 보일때만 재생(숨길때 멈춤)
-
-    3. 배너 터치기능 넘기기 (제이쿼리 UI이용)
-    + 배너별 타이틀 등장하기
-    + 양쪽 이동버튼 플러그인 적용하기
-
-*************************************************/
-
-// 자동스크롤 기능 함수 가져오기
-import autoScroll from "./jquery-autoScroll.js";
 // 메뉴기능함수 가져오기
 import menuFn from "./mainjs/menu.js";
-// 배너기능함수 가져오기
-import banFn from "./mainjs/ban.js";
 // 공통 데이터 가져오기
 import comData from "./tempData/data-common.js";
 
@@ -28,7 +9,7 @@ import comData from "./tempData/data-common.js";
 //###### 상단영역 메뉴 뷰 템플릿 셋팅하기 #######
 // Vue.component(내가지은요소명,{옵션})
 Vue.component("top-comp",{
-    template:comData.tarea,
+    template:comData.tareaSub,
 }); ////////// 상단영역 Vue component //////////
 
 //###### 하단영역 메뉴 뷰 템플릿 셋팅하기 #######
@@ -45,14 +26,15 @@ new Vue({
     mounted:function(){
         // 제이쿼리코드함수 호출!
         console.log("mounted구역");
-        
-        // 자동스크롤 호출
-        autoScroll();
+
         // 메뉴기능 호출
         menuFn();
-        // 배너기능 호출
-        banFn();
-        
+
+        // 스와이퍼 생성함수 호출
+        makeSwiper();
+
+        // 부드러운 스크롤 실행
+        startSS();
     },    
     // created 실행구역 : DOM연결전
     created:function(){
@@ -69,4 +51,28 @@ new Vue({
 
 
 
+// 스와이퍼 플러그인 인스턴스 생성하기 ///
+// 스와이퍼 생성함수
+function makeSwiper(){
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+        // 인터렉션 비활성화 false 
+        // -> 인터렉션 활성화! (가만히두면 다시자동넘김)
+      },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true, // 블릿클릭이동여부
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+} /////////// makeSwiper 함수 ///////////
 
