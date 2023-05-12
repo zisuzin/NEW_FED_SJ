01bb358f68ad1e088500b4492572b5fe3711f5f1
zisuzin <zi_soo_zin@naver.com>
2023-05-12 10:19:04 +0900

    제이슨 연습

---
+++ 010.VueJS학습/001.기초연습/js/vueData-store.js
@@ -0,0 +1,16 @@
+// 뷰JS 데이터처리 뷰엑스 스토어 JS - vuedata-store.js 
+const store = new Vuex.Store({
+    // (1) 데이터 셋팅구역 :
+    state:{
+
+    }, // state 구역 //
+
+    // (2) 데이터 변경 메서드 구역
+    mutations:{
+
+    }, // mutations 구역 // 
+}); // 뷰엑스 인스턴스 //
+
+
+// 내보내기 
+export default store;
