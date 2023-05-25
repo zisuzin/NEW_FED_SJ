dced7d3899406f212b7a82fc99738ec253b15171
zisuzin <zi_soo_zin@naver.com>
2023-05-25 17:19:24 +0900

    ss

--- 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-router.js
+++ 700.쉐도잉프로젝트/03.Pilot_PJ/002.구현소스/js/glist-router.js
@@ -27,14 +27,23 @@ let Glist = {
             <!-- 상품리스트박스 -->
             <div class="grid">
                 <div 
-                v-for="
-                    (v,i) in $store.state.gdata
-                "
-                v-if="
-                v.cat==$store.state.selnm[0] ||
-                v.cat==$store.state.selnm[1] ||
-                v.cat==$store.state.selnm[2]
+                    v-for="
+                        (v,i) in $store.state.gdata
+                    "
+                    v-if="
+                        v.cat==$store.state.selnm[0] ||
+                        v.cat==$store.state.selnm[1] ||
+                        v.cat==$store.state.selnm[2]
                 ">
+
+                
+                <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
+                <router-link 
+                    v-bind:to="{
+                        name:'det',
+                        params:{id : v.idx, list : $route.path}
+                    }">
+                [{{v.idx}}]
                     <img 
                         v-bind:src="
                         './images/goods/'+
@@ -45,6 +54,11 @@ let Glist = {
                         <h2>{{v.ginfo[1]}}</h2>
                         <h3>{{v.ginfo[3]}}</h3>
                     </aside>
+
+
+                </router-link>
+
+                    
                 </div>
             </div>
         </section>
@@ -74,7 +88,17 @@ let Paging = {
                 v.idx >= 1 + $store.state.pnum && 
                 v.idx <= 10 + $store.state.pnum
             ">
-                [{{v.idx}}]<img 
+
+               
+            <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
+            <router-link 
+                v-bind:to="{
+                    name:'det',
+                    params:{id : v.idx, list : $route.path}
+                }">
+                [{{v.idx}}]
+                
+                <img 
                     v-bind:src="
                     './images/goods/'+
                     v.cat +
@@ -84,6 +108,9 @@ let Paging = {
                     <h2>{{v.ginfo[1]}}</h2>
                     <h3>{{v.ginfo[3]}}</h3>
                 </aside>
+
+            </router-link>
+
             </div>
         </div>
 
@@ -123,7 +150,18 @@ let More = {
                 v.idx >= 1 && 
                 v.idx <= 5 + $store.state.mnum
             ">
-                [{{v.idx}}]<img 
+
+               
+            <!-- 파라미터가 있는 뷰라우터는 이름으로 호출! -->
+            <router-link 
+                v-bind:to="{
+                    name:'det',
+                    params:{id : v.idx, list : $route.path}
+                }">
+                    
+                [{{v.idx}}]
+                
+                <img 
                     v-bind:src="
                     './images/goods/'+
                     v.cat +
@@ -133,6 +171,8 @@ let More = {
                     <h2>{{v.ginfo[1]}}</h2>
                     <h3>{{v.ginfo[3]}}</h3>
                 </aside>
+            </router-link>
+
             </div>
         </div>
 
@@ -149,13 +189,15 @@ let More = {
 `,
 };
 
+// 상세보기 구성 템플릿
 const Detail = {
     template: `
             
     <!-- 큰이미지 배경박스 -->
     <div id="bgbx">
         <!-- 닫기버튼 -->
-        <a href="#" class="cbtn" @click="$router.push('/glist')">
+        <a href="#" class="cbtn" 
+        @click="$router.push($route.params.list)">
             <span class="ir">닫기버튼</span>
         </a>
         
@@ -164,15 +206,36 @@ const Detail = {
             <div class="inx">
                 <!-- 큰 이미지 -->
                 <section class="gimg">
-                    <img class="magnify" :src="
-                    'images/goods/'+$store.state.gdata[$route.params.id-1].cat+
-                    '/'+$store.state.gdata[$route.params.id-1].ginfo[0]+'.png'
+                    <img class="magnify" 
+
+                    v-bind:src="
+                    'images/goods/'+
+
+                    <!-- 카테고리명(men/women/style) -->
+                    $store.state.gdata[$route.params.id-1].cat+
+
+                    '/'+
+                    
+                    <!-- 이미지명(클래스명) -->
+                    $store.state.gdata[$route.params.id-1].ginfo[0]+
+                    
+                    '.png'
                     " alt="큰 이미지">
+
                     <div class="small">
                         <a href="#">
-                        <img v-for="v in 6" :src="
-                        'images/goods/'+$store.state.gdata[$route.params.id-1].cat+
+
+                        <img v-for="v in 6" 
+
+                        v-bind:src="
+
+                        'images/goods/'+
+
+                        <!-- 카테고리명(men/women/style) -->
+                        $store.state.gdata[$route.params.id-1].cat+
+
                         '/m'+v+'.png'
+
                         " alt="작은 이미지"></a>
                     </div>
                 </section>
@@ -186,7 +249,11 @@ const Detail = {
                             <li>
                                 <img src="images/dx_ico_new-28143800.gif" alt="new버튼">
                             </li>
-                            <li id="gtit">상품명 : {{$store.state.gdata[$route.params.id-1].ginfo[1]}}</li>
+                            <li id="gtit">상품명 :
+
+                            {{$store.state.gdata[$route.params.id-1].ginfo[1]}}
+                            
+                            </li>
                             <li>
                                 <img src="images/icon_type02_social01.gif" alt="페이스북"><img
                                     src="images/icon_type02_social02.gif" alt="트위터"><img src="images/icon_mail02.gif"
@@ -194,7 +261,11 @@ const Detail = {
                             </li>
                             <li>
                                 <span>판매가</span>
-                                <span id="gprice">{{$store.state.gdata[$route.params.id-1].ginfo[3]}}</span>
+                                <span id="gprice">
+
+                                {{$store.state.gdata[$route.params.id-1].ginfo[3]}}
+
+                                </span>
                             </li>
                             <li>
                                 <span>적립금</span>
@@ -206,7 +277,11 @@ const Detail = {
                             </li>
                             <li>
                                 <span>상품코드</span>
-                                <span id="gcode">{{$store.state.gdata[$route.params.id-1].ginfo[2]}}</span>
+                                <span id="gcode">
+                                
+                                {{$store.state.gdata[$route.params.id-1].ginfo[2]}}
+                                
+                                </span>
                             </li>
                             <li>
                                 <span>사이즈</span>
@@ -244,15 +319,21 @@ const Detail = {
                     <div>
                         <!--버튼영역-->
                         <button class="btn btn1">BUY NOW</button>
-                        <button class="btn scbtn" @click="$store.state.resCheck">SHOPPING CART</button>
-                        <button class="btn">WISH LIST</button>
+                        <button class="btn scbtn"
+                        @click="$store.commit('setData',$route.params.id-1)"
+                        >SHOPPING CART</button>
+                        <button class="btn"
+                        @click="$store.commit('clearData')"
+                        >CLEAR CART</button>
+
                     </div>
                 </section>
             </div>
         </div>
     </div>
-    `
-}
+`,
+};
+
 
 // 뷰라우터 인스턴스 생성하기
 const router = new VueRouter({
@@ -273,13 +354,21 @@ const router = new VueRouter({
             component: More,
         },
         // 4. 상세보기 : 파라미터 필요! - 상품구분 idx로사용
-        // {
-        //     // 파라미터를 쓰려면 뷰라우터 이름이 필수
-        //     name: "det",
-        //     path: "/more",
-        //     component: Detail,
-        // },
-    ],
+        {
+            // 파라미터를 쓰려면 뷰라우터 이름이 필수
+            name : "det",
+            path:"/detail",
+            component: Detail,
+        },
+    ], /// routes 끝
+
+    
+    // 라우터 호출시 스크롤위치 최상단 자동이동코드!!!
+    // 새로 라우팅을 할때마다 이 스크롤 메서드가 호출되어 실행됨!
+    scrollBehavior (to, from, savedPosition) {
+        return { x: 0, y: 0 }
+    }
+
 });
 
 // 내보내기
