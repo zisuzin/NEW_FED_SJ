1c640aef227f2cf510b577e1aecbc7e3223cba7e
zisuzin <zi_soo_zin@naver.com>
2023-05-30 11:46:15 +0900

    async/await

--- 005.jQuery학습/003.응용구현/Megabox_PJ/js/main.js
+++ 005.jQuery학습/003.응용구현/Megabox_PJ/js/main.js
@@ -68,6 +68,26 @@ $(function () { //// jQB1 //////////////////
 
     }); ///////////// click ///////////////
 
+    // 2-2. 소리남/안남 기능 
+    // 대상 : .btnsnd img
+    // 원리 : 소리가 나는지 안나는지 속성 -> muted 사용해서 반대로 전환
+    // 핵심 : 현재 소리가 안나는지 상태를 확인함 
+    $(".btnsnd img").click(function(){
+        // 1. 현재 소리가 안나는지 상태 알아오기
+        // 동영상 소리 안남 여부 설정 -> muted 
+        let sound = mv.get(0).muted;
+        console.log("소리안나나?", sound);
+
+        // 2. 만약 소리가 안나면 나게 / 나면 안나게하기
+        // muted 속성은 현재소리안남 상태값을 불린으로 읽기/쓰기 가능
+        mv.get(0).muted = !sound;
+        // !sound 는 true/false 전환 코드임!
+
+        // 3. 아이콘을 현재 소리상태로 넣기
+        // sound가 true이면 반대로 했으므로 소리남 아이콘!
+        sound ? $(this).attr("src","./images/speaker_blue.png") : $(this).attr("src","./images/speaker-mute_blue.png");
+    }); // click 
+
 
 
 }); ///////////// jQB1 ///////////////////
