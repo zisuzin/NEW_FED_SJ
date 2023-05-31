eb5f6d68b3b2a3dd2a4190465002961d141bdda2
zisuzin <zi_soo_zin@naver.com>
2023-05-31 16:58:00 +0900

    ss

---
+++ 014.React학습/react-app/sj-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Test from './Test';
+
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root"));
+root.render(<Test />);
