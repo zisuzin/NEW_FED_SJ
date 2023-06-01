0283664f3d33d2fe3682808b1f6b345d66b04302
zisuzin <zi_soo_zin@naver.com>
2023-06-01 14:49:09 +0900

    ss

---
+++ 014.React학습/react-app/sj-app/src/dc/Ban.js
@@ -0,0 +1,24 @@
+
+
+function Ban() {
+    return (
+        <div className="banner">
+            <ul className="slider">
+                <li>
+                    <img src="../src/images/dcm21.jpg" alt="배너" />
+                    <section className="bantit">
+                        <h3>GOTHAM GAZETTE</h3>
+                        <h2>WORLDS TRAVELER</h2>
+                        <p>
+                            Barry who? The Flash isn't the only DC hero this summer who's been traveling
+                            through the multiverse.
+                        </p>
+                        <button>New Places, Familiar Faces</button>
+                    </section>
+                </li>
+            </ul>
+        </div>
+    );
+} /////////// Ban 컴포넌트 /////////////
+
+export default Ban;
