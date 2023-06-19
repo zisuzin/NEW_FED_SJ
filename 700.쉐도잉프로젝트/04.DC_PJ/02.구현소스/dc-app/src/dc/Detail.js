73503091f7e1536206b47f8f60ab01a0ab88b196
zisuzin <zi_soo_zin@naver.com>
2023-06-19 12:24:54 +0900

    ss

---
+++ 700.쉐도잉프로젝트/04.DC_PJ/02.구현소스/dc-app/src/dc/Detail.js
@@ -0,0 +1,34 @@
+// 상세페이지 컴포넌트 - Detail.js
+import { useLocation } from "react-router-dom";
+
+// 라우터 파라미터값 받아서 데이터 처리!
+
+function Detail(props){
+    // 라우터 전달값을 받기위한 useLocation 생성하기!
+    const loc = useLocation();
+    // 보내 속성명을 변수에 할당하기
+    // 1. 캐릭터이름
+    const cname = loc.state.cname; 
+    // 2. 캐릭터설명
+    let cdesc = loc.state.cdesc;
+    cdesc = cdesc.split('^');
+    // 3. 캐릭터명세
+    const facts = loc.state.facts;
+
+    return (
+        <>
+            <h2>{cname}</h2>
+            <div className="cdesc">
+                {
+                    cdesc.map(v=><p>{v}</p>)
+                }
+            </div>
+            <div className="facts">
+                <h3>CHARACTER FACTS</h3>
+                {facts}
+            </div>
+        </>
+    )
+} // Detail //
+
+export default Detail;
